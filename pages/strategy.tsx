2566015971f3ec7ea4147e852f42c04a02df8143
cheman <chemanlau@gmail.com>
2026-04-29 07:18:47 +0800

    Update strategy.tsx

--- pages/strategy.tsx
+++ pages/strategy.tsx
@@ -16,7 +16,7 @@ const STRATEGY_PARAMS_CACHE_KEY = 'strategy_params_cache';
 // 默认策略参数
 const DEFAULT_STRATEGY_PARAMS: StrategyParams = {
     initial_capital: 50000,
-    grid_width: 2.5,
+    grid_width: 0.025,
     num_grids: 20,
     grid_investment_percent: 10,  // 默认每格投资5%的资金
     use_volatility_adjustment: true,
